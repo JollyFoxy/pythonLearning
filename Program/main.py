155f864cba9509641e703fa9179f0f685a16f6ed
pavel <pavel.mizirev@gmail.com>
2023-03-06 17:46:47 +0300

    ЧЗХ

--- Program/main.py
+++ Program/main.py
@@ -1,12 +1,33 @@
 class Person:
-    def __init__(self, name, age, phone):
+    def __init__(self, name):
         self.name = name
+        self.age = 1
+        self.phone = "null"
+
+    @property
+    def get_age(self):
+        return self.age
+
+    @get_age.setter
+    def set_age(self, age):
         self.age = age
+
+    @property
+    def get_phone(self):
+        return self.phone
+
+    @get_phone.setter
+    def set_phone(self, phone):
         self.phone = phone
 
+    @property
+    def get_name(self):
+        return self.name
     def display_info(self):
         print(f"Name: {self.name}\nAge: {self.age}\nPhone: {self.phone}")
 
 
-viktor = Person("vitya", 24, "88005553535")
+viktor = Person("Vitya")
+viktor.set_age()
+viktor.set_phone(88005553535)
 viktor.display_info()
