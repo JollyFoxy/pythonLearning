29dc434f4620cc64ac5094ef78cf539dfb15851a
pavel <pavel.mizirev@gmail.com>
2023-03-07 16:48:47 +0300

    Первый тостик

--- Program/Person.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self, name, age, phone):
-        self.name = name
-        self.age = age
-        self.phone = phone
-
-    def display_info(self):
-        print(f"Name: {self.name}\nAge: {self.age}\nPhone: {self.phone}")
